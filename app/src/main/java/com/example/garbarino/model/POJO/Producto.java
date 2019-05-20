16c57007c791eb2bdb3bf6732b898b9b049751b2
romina793 <rominapozzuto@gmail.com>
2019-05-20 11:11:52 -0300

    recycler ok + description y price

--- app/src/main/java/com/example/garbarino/model/POJO/Producto.java
+++ app/src/main/java/com/example/garbarino/model/POJO/Producto.java
@@ -73,5 +73,15 @@ public class Producto {
     }
 
 
+    public Producto( String id, String description, String imageUrl, Integer price, Integer listPrice, Integer discount ) {
+        this.id = id;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.listPrice = listPrice;
+        this.discount = discount;
+    }
 
+    public Producto() {
+    }
 }
--- app/src/main/java/com/example/garbarino/view/ProductoAdapter.java
+++ app/src/main/java/com/example/garbarino/view/ProductoAdapter.java
@@ -5,10 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.garbarino.R;
 import com.example.garbarino.model.POJO.Producto;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +19,23 @@ import java.util.List;
 public class ProductoAdapter extends RecyclerView.Adapter  {
 
     private List<Producto> listaProductos;
+    private ListenerProductoAdapter listenerProductoAdapter;
 
     public ProductoAdapter() {
-        listaProductos = new ArrayList<>();
+        this.listaProductos = new ArrayList<>();
     }
 
-    public void setListaProductos(List<Producto> listaProductos) {
+    public void setListaProductos( List<Producto> listaProductos) {
         this.listaProductos = listaProductos;
         notifyDataSetChanged();
+    }
+
+    public ListenerProductoAdapter getListenerProductoAdapter() {
+        return listenerProductoAdapter;
+    }
+
+    public void setListenerProductoAdapter( ListenerProductoAdapter listenerProductoAdapter ) {
+        this.listenerProductoAdapter = listenerProductoAdapter;
     }
 
     @NonNull
@@ -50,15 +61,38 @@ public class ProductoAdapter extends RecyclerView.Adapter  {
     private class ViewHolderProducto extends RecyclerView.ViewHolder{
 
         private TextView textViewDescriptionProducto;
+        private TextView textViewPrecio;
+        private ImageView imageViewproducto;
+        private Producto producto;
 
         public ViewHolderProducto(View itemView) {
             super(itemView);
             textViewDescriptionProducto = itemView.findViewById(R.id.celda_nombreProducto);
+            imageViewproducto = itemView.findViewById(R.id.imageViewCelda);
+            textViewPrecio = itemView.findViewById(R.id.textViewPrecio);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick( View v ) {
+               // listenerProductoAdapter.informarSeleccion(producto);
+                }
+            });
         }
 
 
         public void loadProducto(Producto producto){
+            this.producto = producto;
             textViewDescriptionProducto.setText(producto.getDescription());
+            textViewPrecio.setText("$ " + producto.getPrice().toString() + ".-");
+            if (producto.getImageUrl() == null){
+                imageViewproducto.setImageResource(R.drawable.imagengarbarino);
+            }else {
+            Picasso.get().load(producto.getImageUrl()).into(imageViewproducto);
+            }
         }
+    }
+
+
+    public interface ListenerProductoAdapter {
+        public void informarSeleccion (Producto producto);
     }
 }
